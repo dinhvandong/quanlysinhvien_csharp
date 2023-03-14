bb45d22d819c7cda80ebc5288f537cc44a99daa9
deepcode1-admin <dongdinhvan.edu@gmail.com>
2023-03-14 15:13:15 +0700

    Upload new source code studentmanagement

---
+++ StudentManagement/StudentManagement/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Student> listStudent = new List<Student>();
+
+            Student s1 = new Student(1, "Nam",4,5,6);
+            Student s2 = new Student(2, "Linh",3,5,6);
+            Student s3 = new Student(3, "Quynh",9,7,8);
+            Student s4 = new Student(4, "Manh",6,8,9);
+            Student s5 = new Student(5, "Loc",5,1,1);
+            Student s6 = new Student(6, "Giang",7,8,9);
+            Student s7 = new Student(7, "Tung",9,9,9);
+            Student s8 = new Student(8, "Anh",6,6,3);
+
+            listStudent.Add(s4);
+            listStudent.Add(s8);
+            listStudent.Add(s1);
+            listStudent.Add(s3);
+            listStudent.Add(s5);
+            listStudent.Add(s6);
+            listStudent.Add(s7);
+            listStudent.Add(s2);
+
+
+            Console.WriteLine("Before Sort");
+            foreach (Student s in listStudent)
+            {
+
+                s.ToString();
+            }
+
+            listStudent.Sort((a, b) => 
+            (a.Math+a.Physic +a.History).CompareTo(b.Math + b.Physic + b.History));
+
+            Console.WriteLine("After Sort");
+            foreach(Student s in listStudent)
+            {
+
+                s.ToString();
+            }
+
+        }
+    }
+}
---
+++ StudentManagement/StudentManagement/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement
+{
+    internal class Student
+    {
+
+        public int StudentID { get; set; }  
+        public string Name { get; set; }
+
+        public float Math { get; set; }
+
+        public float History { get; set; }
+
+        public float Physic { get; set; }   
+
+
+        public Student() { }
+
+        public Student(int studentID, string name)
+        {
+            StudentID = studentID;
+            Name = name;
+        }
+
+        public Student(int studentID, string name, 
+            float math, float physic, float history)
+        {
+            StudentID = studentID;
+            Name = name;
+            Math = math;
+            Physic = physic;
+            History = history;
+
+        }
+
+
+        public void ToString()
+        {
+            Console.WriteLine("StudentID:" + StudentID 
+                + "Name:" +Name  + "SUM:"
+                +(this.Math + this.History + this.Physic));
+        }
+
+    }
+}
